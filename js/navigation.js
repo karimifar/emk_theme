7008b2f612fa62a5ba408f4c8f236bba1d2efeb6
Em Karimifar <em.karimifar@gmail.com>
2019-12-31 01:00:47 -0600

    header scrolled

--- js/navigation.js
+++ js/navigation.js
@@ -104,3 +104,23 @@
 		}
 	}( container ) );
 } )();
+
+
+
+var scroll_pos = window.scrollY;
+console.log(scroll_pos)
+window.addEventListener('scroll', function() {
+	scroll_pos = window.scrollY;
+	console.log(scroll_pos)
+	add_header_class(scroll_pos)
+
+});
+
+function add_header_class(scrollPos){
+	if(scrollPos >= 40){
+		console.log("cloic")
+		$("body").addClass("scrolled")
+	}else if (scrollPos<40){
+		$("body").removeClass("scrolled")	
+	}
+}
