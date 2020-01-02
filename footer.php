7bd752ea08c616040b895415839b851095d740d4
Em Karimifar <em.karimifar@gmail.com>
2020-01-01 23:16:32 -0600

    works-single

--- footer.php
+++ footer.php
@@ -14,19 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'emk_theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'emk_theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'emk_theme' ), 'emk_theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+	©2019 All rights reserved | Em Karimifar
 	</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
--- single-works.php
+++ single-works.php
@@ -6,15 +6,67 @@
  *
  * @package emk_theme
  */
-wp_enqueue_style( 'work-single', get_template_directory_uri() . "/css/work-single/work.css", array('emk_styles') );
-wp_enqueue_script( 'work-scripts', get_template_directory_uri() . "/js/work-single/work.js", array('jquery'), null, true );
+    wp_enqueue_style( 'work-single', get_template_directory_uri() . "/css/work-single/work.css", array('emk_styles') );
+    wp_enqueue_script( 'work-scripts', get_template_directory_uri() . "/js/work-single/work.js", array('jquery'), null, true );
 
-get_header();
-?>
+    get_header();
+
+    create_work_page();
+
+    function create_work_page(){
+        
+        echo '<div class="full-col column title-row">';
+            echo '<h1 class="page-title">'.get_the_title().'</h1>';
+        echo '</div>';
+        echo '<div class="col-35 column work-desc">';
+            get_work_description();
+        echo '</div>';
+        echo '<div class="col-65 column work-content">';
+            get_work_content();
+        echo '</div>';
+
+    };
+
+
+    function get_work_description(){
+        the_post();
+        echo '<div class="desc-content">';
+            the_content();
+        echo '</div>';
+        
+    }
+
+    function get_work_content(){
+        // if( have_rows('project_images') ):
+
+        //    while ( have_rows('project_images') ) : the_row();
+       
+        //        $imageCaption = get_sub_field('image_caption');
+        //        $imageUrl = get_sub_field('image_url');
+
+        //        echo '<img src="' . $imageUrl .'" alt="">';
+        //        echo $imageCaption;
+       
+        //    endwhile;
+
+        // endif;
+
+
+        $imageGallery = get_field("image_gallery");
+        if ($imageGallery){
+            foreach($imageGallery as $imageArr){
+                echo '<div class="work-img-wrap">';
+                echo '<img src="' . $imageArr['url'] .'" alt="">';
+                echo '<p>' . $imageArr['caption'] .'</p>';
+                echo '</div>';
+            }
+            
+        }
+
+    }
 
 
 
-<?php
     get_footer();
 ?>
 
